9b7854ec3414a07eededbe0ed82f048b13c0dc28
dudska12 <skadud0113@naver.com>
2026-01-20 20:46:50 +0900

    레포지토리 User Email 찾기

---
+++ src/auth/auth.repository.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+@Injectable()
+export class AuthRepository {
+  constructor(private prisma: PrismaService) {}
+
+  async findUserByEmail(email: string) {
+    return this.prisma.user.findUnique({
+      where: { email },
+    });
+  }
+  
+}
